7c53efb87021cd42103f98301df17ca0f52d76d2
Nikolas <nwijaya@treeclouds.com>
2023-08-31 09:18:27 +0700

    on progress

---
+++ src/components/Spotlight/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { API } from "@stoplight/elements";
+import '@stoplight/elements/styles.min.css';
+
+const Spotlight = ({ apiDescriptionUrl }) => {
+  return (
+    <div>
+      {/* <API apiDescriptionUrl={apiDescriptionUrl} router="hash" /> */}
+    </div>
+  );
+};
+
+export default Spotlight;
--- src/pages/api-reference/index.js
+++ src/pages/api-reference/index.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Layout from "@theme/Layout";
 import { Wrapper } from "./_element";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export default function Hello() {
+  const LazyStoplight = React.lazy(() => import("../../components/Spotlight"));
+  const noIdeaForWhat = (
+    <div style={{ minHeight: "calc(100vh - var(--ifm-navbar-height))" }} />
+  );
   return (
     <Layout title="Hello" description="Hello React Page">
-      <Wrapper>
-        <p>Content here</p>
-      </Wrapper>
+      <BrowserOnly>
+        {() => (
+          <Suspense fallback={noIdeaForWhat}>
+            <LazyStoplight apiDescriptionUrl="/openapi/duitnow/duitnow-payment.yml" />
+          </Suspense>
+        )}
+      </BrowserOnly>
     </Layout>
   );
 }
