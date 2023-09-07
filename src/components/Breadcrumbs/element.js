3c09e9974005c83cd72010e4a53a4a9845ba84a2
Putri Theodora <putri@treeclouds.com>
2023-09-07 15:55:05 +0700

    [put-pages] Create cross-border-outward page

--- src/components/Breadcrumbs/element.js
+++ src/components/Breadcrumbs/element.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const BreadCrumbWrapper = styled.div`
   display:flex;
+  flex-wrap: wrap;
   flex-direction:row ;
   align-items:center;
   gap: 0.66rem;
