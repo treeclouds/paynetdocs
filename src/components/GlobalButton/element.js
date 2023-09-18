700132355a24577f62184d1a4dad9aeb55d65e8b
Nikolas <nwijaya@treeclouds.com>
2023-09-18 15:26:04 +0700

    create example global variable

--- src/components/GlobalButton/element.js
+++ src/components/GlobalButton/element.js
@@ -1,14 +1,8 @@
 import styled from 'styled-components'
 
-export const BreadCrumbWrapper = styled.div`
-  display:flex;
-  flex-wrap: wrap;
-  flex-direction:row ;
-  align-items:center;
-  gap: 0.66rem;
-  margin-bottom: 1.33rem;
-
-  @media(min-width: 1024px) {
-    margin-bottom: 1.56rem;
-  }
+export const Wrapper = styled.span`
+ .word{
+  cursor: pointer;
+  color: blue;
+ }
 `
--- src/components/GlobalButton/index.js
+++ src/components/GlobalButton/index.js
@@ -1,13 +1,18 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import React from 'react'
+import { Wrapper } from './element'
 
-const GlobalButton = () => {
+const GlobalButton = ({type="button", label="Click me"}) => {
   const { siteConfig } = useDocusaurusContext()
 
   return (
-    <div>
-      <a href={`/${siteConfig.themeConfig.GLOBAL_URL}`} class="button">Click Me</a>
-    </div>
+    <Wrapper>
+      {type==="button"? 
+      <a href={`/${siteConfig.themeConfig.GLOBAL_URL}`} className="button">{label}</a>
+      : 
+      <a href={`/${siteConfig.themeConfig.GLOBAL_URL}`} className="word">{label}</a>
+      }
+    </Wrapper>
   )
 }
 export default GlobalButton
